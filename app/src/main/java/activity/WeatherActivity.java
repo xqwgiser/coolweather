132ce98a4acbd43a74ee00a396fb147e3577d709
XQW <864722821@qq.com>
2015-05-07 22:21:04 +0800

    加入天气图片。

--- app/src/main/java/activity/WeatherActivity.java
+++ app/src/main/java/activity/WeatherActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,6 +23,21 @@ import util.Utility;
  * Created by xqw on 2015/5/5.
  */
 public class WeatherActivity extends Activity implements View.OnClickListener {
+    public static final int Qing=0;
+    public static final int Yin=1;
+    public static final int Duoyun=2;
+    public static final int Xiaoyu=3;
+    public static final int Zhongyu=4;
+    public static final int Dayu=5;
+    public static final int Baoyu=6;
+    public static final int Zhenyu=7;
+    public static final int Leizhenyu=8;
+    public static final int Yujiaxue=9;
+    public static final int Xiaoxue=10;
+    public static final int Zhongxue=11;
+    public static final int Daxue=12;
+    public static final int Baoxue=13;
+    private int weatherstate;
     private LinearLayout weatherInfoLayout;
     private TextView cityNameText;
     private TextView publishText;
@@ -34,6 +50,7 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
     private TextView ssdText;
     private Button switchCity;
     private Button refreshWeather;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -49,6 +66,7 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         lyText=(TextView)findViewById(R.id.ly_text);
         clText=(TextView)findViewById(R.id.cl_text);
         ssdText=(TextView)findViewById(R.id.ssd_text);
+        imageView=(ImageView)findViewById(R.id.weather_image);
         String countyCode=getIntent().getStringExtra("county_code");
         if(!TextUtils.isEmpty(countyCode)){
             publishText.setText("同步中...");
@@ -133,11 +151,84 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         temp1Text.setText(prefs.getString("temp1",""));
         temp2Text.setText(prefs.getString("temp2",""));
         weatherDespText.setText(prefs.getString("weather_desp",""));
+        String weatherinfo=prefs.getString("weather_desp","");
+        if(weatherinfo.contains("晴"))
+            weatherstate=Qing;
+        else if (weatherinfo.contains("阴"))
+            weatherstate=Yin;
+        else if (weatherinfo.contains("多云"))
+            weatherstate=Duoyun;
+        else if (weatherinfo.contains("小雨"))
+            weatherstate=Xiaoyu;
+        else if (weatherinfo.contains("中雨"))
+            weatherstate=Zhongyu;
+        else if (weatherinfo.contains("大雨"))
+            weatherstate=Dayu;
+        else if (weatherinfo.contains("暴雨"))
+            weatherstate=Baoyu;
+        else if (weatherinfo.contains("阵雨"))
+            weatherstate=Zhenyu;
+        else if (weatherinfo.contains("雷阵雨"))
+            weatherstate=Leizhenyu;
+        else if (weatherinfo.contains("雨夹雪"))
+            weatherstate=Yujiaxue;
+        else if (weatherinfo.contains("小雪"))
+            weatherstate=Xiaoxue;
+        else if (weatherinfo.contains("中雪"))
+            weatherstate=Zhongxue;
+        else if (weatherinfo.contains("大雪"))
+            weatherstate=Daxue;
+        else if (weatherinfo.contains("暴雪"))
+            weatherstate=Baoxue;
         publishText.setText(prefs.getString("publish_time",""));
         currentDateText.setText(prefs.getString("current_date",""));
-        lyText.setText(prefs.getString("ly_text",""));
-        clText.setText(prefs.getString("cl_text",""));
-        ssdText.setText(prefs.getString("ssd_text",""));
+        lyText.setText("旅游："+prefs.getString("ly_text","")+"  ");
+        clText.setText("晨练："+prefs.getString("cl_text","")+"  ");
+        ssdText.setText("舒适度："+prefs.getString("ssd_text","")+"  ");
+        switch (weatherstate){
+            case Qing:
+                imageView.setImageResource(R.drawable.org3_ww0);
+                break;
+            case Yin:
+                imageView.setImageResource(R.drawable.org3_ww2);
+                break;
+            case Duoyun:
+                imageView.setImageResource(R.drawable.org3_ww1);
+                break;
+            case Xiaoyu:
+                imageView.setImageResource(R.drawable.org3_ww7);
+                break;
+            case Zhongyu:
+                imageView.setImageResource(R.drawable.org3_ww8);
+                break;
+            case Dayu:
+                imageView.setImageResource(R.drawable.org3_ww19);
+                break;
+            case Baoyu:
+                imageView.setImageResource(R.drawable.org3_ww10);
+                break;
+            case Zhenyu:
+                imageView.setImageResource(R.drawable.org3_ww3);
+                break;
+            case Leizhenyu:
+                imageView.setImageResource(R.drawable.org3_ww4);
+                break;
+            case Yujiaxue:
+                imageView.setImageResource(R.drawable.org3_ww6);
+                break;
+            case Xiaoxue:
+                imageView.setImageResource(R.drawable.org3_ww14);
+                break;
+            case Zhongxue:
+                imageView.setImageResource(R.drawable.org3_ww15);
+                break;
+            case Daxue:
+                imageView.setImageResource(R.drawable.org3_ww16);
+                break;
+            case Baoxue:
+                imageView.setImageResource(R.drawable.org3_ww17);
+                break;
+        }
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
         Intent intent=new Intent(this, AutoUpdateService.class);
